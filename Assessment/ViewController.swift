5f588b34c77cb086b3ad46fc2e0f940de09fea0a
deepthireddy <deepthi-reddy@apptrinity.com>
2022-12-06 10:29:06 +0530

    Initial Commit

---
+++ Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assessment
+//
+//  Created by deepthi reddy on 06/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
